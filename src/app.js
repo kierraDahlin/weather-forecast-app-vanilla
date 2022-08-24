672d6e975e5848d0edbb1645faa27a2d631a53fb
Kierra Dahlin <kierra@choice.marketing>
2022-08-24 15:41:01 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -48,10 +48,11 @@ function displayForecast(response) {
 //
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "cac27e453346e9164edaf605b6536f2f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
-  // axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemp(response) {
